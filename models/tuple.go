180ff80d3ba70ffce6b207ffcef275df2c7b7b6f
Laila Weil <laila.weil@mercadolibre.com>
2021-06-05 15:04:16 -0300

    adding more Tuple Operations

---
+++ models/tuple.go
@@ -0,0 +1,84 @@
+package models
+
+import "github.com/laiweil/goray-tracer/utils"
+
+type Tuple struct {
+	X float64
+	Y float64
+	Z float64
+	W byte
+}
+
+func Vector(x, y, z float64) *Tuple {
+	return &Tuple{
+		X: x,
+		Y: y,
+		Z: z,
+		W: 0,
+	}
+}
+
+func Point(x, y, z float64) *Tuple {
+	return &Tuple{
+		X: x,
+		Y: y,
+		Z: z,
+		W: 1,
+	}
+}
+
+func (t Tuple) IsVector() bool {
+	return t.W == 0
+}
+
+func (t Tuple) IsPoint() bool {
+	return t.W == 1
+}
+
+func (a Tuple) Equal(b Tuple) bool {
+	return utils.FloatEquals(a.X, b.X) && utils.FloatEquals(a.Y, b.Y) && utils.FloatEquals(a.Z, b.Z) && a.W == b.W
+}
+func (a Tuple) Add(b Tuple) *Tuple {
+	return &Tuple{
+		X: a.X + b.X,
+		Y: a.Y + b.Y,
+		Z: a.Z + b.Z,
+		W: a.W + b.W,
+	}
+}
+
+func (a Tuple) Sub(b Tuple) *Tuple {
+	return &Tuple{
+		X: a.X - b.X,
+		Y: a.Y - b.Y,
+		Z: a.Z - b.Z,
+		W: a.W - b.W,
+	}
+}
+
+func (a *Tuple) Negate() *Tuple {
+	return &Tuple{
+		X: -a.X,
+		Y: -a.Y,
+		Z: -a.Z,
+		W: -a.W,
+	}
+}
+
+func (t Tuple) Multiply(scalar float64) *Tuple {
+	return &Tuple{
+		X: t.X * scalar,
+		Y: t.Y * scalar,
+		Z: t.Z * scalar,
+		W: t.W * byte(scalar),
+	}
+}
+
+func (t Tuple) Divide(scalar float64) *Tuple {
+	return &Tuple{
+		X: t.X / scalar,
+		Y: t.Y / scalar,
+		Z: t.Z / scalar,
+		W: t.W / byte(scalar),
+	}
+}
